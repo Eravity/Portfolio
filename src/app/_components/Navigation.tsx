39d7c9a957fff5bb6828de856a405d5c1514be8f
Eravity <cebotaricristian92@gmail.com>
2024-10-26 19:53:26 +0300

    navbar built

---
+++ src/app/_components/Navigation.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const pathname = usePathname();
+
+  const navItems = [
+    {
+      href: "/",
+      label: "_hello",
+      color: "text-[#F0B65A]",
+      shadow: "shadow-[#F0B65A]",
+    },
+    {
+      href: "/skills",
+      label: "_skills",
+      color: "text-[#99B84A]",
+      shadow: "shadow-[#99B84A]",
+    },
+    {
+      href: "/projects",
+      label: "_projects",
+      color: "text-[#1E95E7]",
+      shadow: "shadow-[#1E95E7]",
+    },
+    {
+      href: "/contacts",
+      label: "_contacts",
+      color: "text-[#C54EE3]",
+      shadow: "shadow-[#C54EE3]",
+    },
+    {
+      href: "/aboutme",
+      label: "_about me",
+      color: "text-[#E43869]",
+      shadow: "shadow-[#E43869]",
+    },
+  ];
+
+  return (
+    <nav className="flex justify-center m-auto my-5 max-w-[700px] py-4 rounded-2xl bg-[#28282E] shadow-lg ">
+      <ul className="flex gap-14 ">
+        {navItems.map((item) => (
+          <li key={item.href}>
+            <Link
+              href={item.href}
+              className={`${
+                pathname === item.href
+                  ? `${item.color}`
+                  : `text-[#B8B8B8] hover:text-[#dddddd] hover:shadow-md hover:${item.shadow} bg-transparent transition duration-200`
+              }`}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
