49febe8eddc18e671086db4a6479b8413f88ec3f
Eravity <cebotaricristian92@gmail.com>
2024-11-06 19:39:18 +0200

    v1.3.0

--- src/app/_components/BentoBox.tsx
+++ src/app/_components/BentoBox.tsx
@@ -17,89 +17,164 @@ const BentoBox: React.FC = (): JSX.Element => {
     Tailwind,
     Typescript,
   } = images;
+
+  const container =
+    "bg-[#28282E] rounded-3xl flex flex-col justify-center items-center shadow-md";
+  const title = "text-[#80808E] text-sm text-center uppercase";
+
   return (
-    // prettier-ignore
     <div
       className="grid gap-2 w-5/6 mx-auto grid-rows-6 h-[760px]"
       style={{ gridTemplateColumns: "10% 10% 20% 15% 28% 15%" }}
     >
       {/* randul 1 */}
-      <div className="rounded-lg col-span-2 row-span-2 h-full">
-        <div className="grid gap-2 grid-cols-2 h-full">
-          <div className="bg-[#28282E] rounded-3xl flex gap-1 flex-col justify-center items-center shadow-md h-[120px] ">
-            <Image src={HTML} alt="Html logo" className="w-16 mt-1"/>
-            <h1 className="text-[#80808E] text-sm">HTML</h1>
+      <div className="rounded-lg col-span-2 row-span-2">
+        <div className="grid gap-2 grid-cols-2">
+          <div className={`${container} gap-1 h-[120px]`}>
+            <Image src={HTML} alt="Html logo" className="w-16 mt-1" />
+            <h1 className={`${title}`}>HTML</h1>
           </div>
-          <div className="bg-[#28282E] rounded-3xl flex gap-1 flex-col justify-center items-center shadow-md h-[120px] ">
-            <Image src={CSS} alt="Html logo" className="w-16 mt-1"/>
-            <h1 className="text-[#80808E] text-sm">CSS</h1>
+          <div className={`${container} gap-1 h-[120px]`}>
+            <Image src={CSS} alt="Html logo" className="w-16 mt-1" />
+            <h1 className={`${title}`}>CSS</h1>
           </div>
-          <div className="bg-[#28282E] flex flex-col justify-center items-center rounded-3xl shadow-md h-[120px] col-span-2 ">
-            <Image src={Supabase} alt="Supabase logo" className="w-60"/>
-            <h1 className="text-[#80808E] text-sm">Supabase</h1>
+          <div className={`${container} h-[120px] col-span-2`}>
+            <Image src={Supabase} alt="Supabase logo" className="w-60" />
+            <h1 className={`${title}`}>Supabase</h1>
           </div>
         </div>
       </div>
 
-      <div className="bg-[#28282E] gap-3 rounded-3xl flex flex-col justify-center items-center shadow-md row-span-2 h-full">
-        <Image src={NextJs} alt="NextJS logo" className="w-44"/>
-        <h1 className="text-[#80808E] text-sm">NextJS</h1>
+      <div className={`${container} gap-3 row-span-2`}>
+        <Image src={NextJs} alt="NextJS logo" className="w-44" />
+        <h1 className={`${title}`}>NextJS</h1>
       </div>
-      <div className="bg-[#28282E] rounded-3xl gap-3 flex flex-col justify-center items-center shadow-md row-span-2 h-full">
-        <Image src={GitHub} alt="GitHub logo" className="w-40"/>
-        <h1 className="text-[#80808E] text-sm">GitHub</h1>
+      <div className={`${container} gap-3 row-span-2`}>
+        <Image src={GitHub} alt="GitHub logo" className="w-40" />
+        <h1 className={`${title} mt-2`}>GitHub</h1>
       </div>
-      <div className="bg-[#28282E] rounded-3xl shadow-md row-span-2 gap-2 flex flex-col justify-center items-center h-full">
-        <Image src={Tailwind} alt="Tailwind logo" className="w-72"/>
-        <h1 className="text-[#80808E] text-sm">Tailwind</h1>
+      <div className={`${container} gap-3 row-span-2`}>
+        <Image src={Tailwind} alt="Tailwind logo" className="w-72" />
+        <h1 className={`${title}`}>Tailwind</h1>
       </div>
-      <div className="bg-[#28282E] rounded-3xl shadow-md row-span-2 flex items-center justify-center h-full">
-        <h1 className="text-[84px] font-bold">SPA</h1>
+      <div className={`${container} row-span-2`}>
+        <h1
+          className="text-[84px] font-bold"
+          style={{
+            background: "linear-gradient(90deg, #E43869, #28282E)",
+            WebkitBackgroundClip: "text",
+            WebkitTextStroke: "8px transparent",
+            color: "#28282E",
+          }}
+        >
+          SPA
+        </h1>
+        <h1 className={`${title} w-[130px]`}>Single Page Application</h1>
       </div>
 
       {/* randul 2 */}
       <div className="grid grid-cols-10 gap-2 col-span-full row-span-2">
-        <div className="bg-[#28282E] rounded-3xl shadow-md col-span-3 h-full">
-          <Image src={Javascript} alt="JavaScript logo" className="w-48"/>
+        <div className={`${container} gap-3 col-span-3`}>
+          <Image src={Javascript} alt="JavaScript logo" className="w-40" />
+          <h1 className={`${title}`}>JavaScript</h1>
         </div>
-        <div className="bg-[#28282E] rounded-3xl shadow-md col-span-4 h-full">
-          <Image src={React} alt="React logo" className="w-52"/>
+        <div className={`${container} col-span-4`}>
+          <Image src={React} alt="React logo" className="w-48" />
+          <h1 className={`${title}`}>React</h1>
         </div>
         <div className="grid col-span-2 gap-2">
-          <div className="bg-[#28282E] rounded-3xl shadow-md">
-            <Image src={Redux} alt="Redux logo"className="w-28"/>
+          <div className={`${container}`}>
+            <Image src={Redux} alt="Redux logo" className="w-24" />
+            <h1 className={`${title}`}>Redux</h1>
           </div>
-          <div className="bg-[#28282E] rounded-3xl shadow-md">
-            <h1 className="text-[64px] font-bold">30+</h1>
+          <div className={`${container}`}>
+            <h1
+              className="text-[64px] ml-5 font-bold"
+              style={{
+                background: "linear-gradient(90deg, #E43869, #28282E)",
+                WebkitBackgroundClip: "text",
+                WebkitTextStroke: "8px transparent",
+                color: "#28282E",
+              }}
+            >
+              30+
+            </h1>
+            <h1 className={`${title} mt-[-10px]`}>Completed projects</h1>
           </div>
         </div>
-        <div className="bg-[#28282E] rounded-3xl shadow-md col-span-1 h-full">
-          <Image src={SQL} alt="SQL Logo" className="w-24"/>
+        <div className={`${container} gap-3`}>
+          <Image src={SQL} alt="SQL Logo" className="w-24" />
+          <h1 className={`${title}`}>SQL</h1>
         </div>
       </div>
 
       {/* randul 3 */}
-      <div className="grid gap-2 col-span-full row-span-2" 
-           style={{ gridTemplateColumns: "minmax(15%, 1fr) minmax(25%, 1fr) minmax(15%, 1fr) minmax(25%, 1fr) minmax(15%, 1fr)" }}>
-        <div className="bg-[#28282E] rounded-3xl shadow-md flex justify-center items-center h-full">
-          <h1 className="text-[84px] font-bold">API</h1>
+      <div
+        className="grid gap-2 col-span-full row-span-2"
+        style={{
+          gridTemplateColumns:
+            "minmax(15%, 1fr) minmax(25%, 1fr) minmax(15%, 1fr) minmax(25%, 1fr) minmax(15%, 1fr)",
+        }}
+      >
+        <div className={`${container}`}>
+          <h1
+            className="text-[84px] font-bold"
+            style={{
+              background: "linear-gradient(90deg, #99B84A, #28282E)",
+              WebkitBackgroundClip: "text",
+              WebkitTextStroke: "8px transparent",
+              color: "#28282E",
+            }}
+          >
+            API
+          </h1>
+          <h1 className={`${title} mt-[-15px]`}>REST API</h1>
         </div>
-        <div className="grid col-span-1 gap-2">
-          <div className="bg-[#28282E] rounded-3xl flex justify-center items-center shadow-md">
-            <h1 className="text-[44px] font-bold ">PERFORMANCE</h1>
+        <div className="grid gap-2">
+          <div className={`${container} gap-1`}>
+            <h1
+              className="text-[44px] font-bold"
+              style={{
+                background: "linear-gradient(90deg, #C54EE3, #28282E)",
+                WebkitBackgroundClip: "text",
+                WebkitTextStroke: "8px transparent",
+                color: "#28282E",
+              }}
+            >
+              PERFORMANCE
+            </h1>
+            <h1 className={`${title}`}>Optimization techniques</h1>
           </div>
-          <div className="bg-[#28282E] rounded-3xl shadow-md">
-            <Image src={Router} alt="Router Logo"/>
+          <div className={`${container} gap-1`}>
+            <Image src={Router} alt="Router Logo" className="w-24" />
+            <h1 className={`${title}`}>Router</h1>
           </div>
         </div>
-        <div className="bg-[#28282E] rounded-3xl shadow-md flex justify-center items-center h-full">
-          <Image src={ssr_csr} alt="SSR_CSR Logo" className="w-40"/>
+        <div className={`${container} gap-3`}>
+          <Image src={ssr_csr} alt="SSR_CSR Logo" className="w-40" />
+          <h1 className={`${title} w-[130px]`}>
+            Server & client side rendering
+          </h1>
         </div>
-        <div className="bg-[#28282E] rounded-3xl shadow-md h-full">
-          <Image src={Typescript} alt="Typescript Logo" className="w-40"/>
+        <div className={`${container} gap-4`}>
+          <Image src={Typescript} alt="Typescript Logo" className="w-40" />
+          <h1 className={`${title}`}>Typescript</h1>
         </div>
-        <div className="bg-[#28282E] rounded-3xl shadow-md h-full">
-          <h1 className="text-[84px] font-bold">SEO</h1>
+        <div className={`${container}`}>
+          <h1
+            className="text-[84px] font-bold"
+            style={{
+              background: "linear-gradient(90deg, #99B84A, #28282E)",
+              WebkitBackgroundClip: "text",
+              WebkitTextStroke: "8px transparent",
+              color: "#28282E",
+            }}
+          >
+            SEO
+          </h1>
+          <h1 className={`${title} w-[120px] mt-[-15px]`}>
+            Search engine optimizaton
+          </h1>
         </div>
       </div>
     </div>
