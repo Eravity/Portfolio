fd918ea4934f9ad245a272f628068601f8bdc383
Eravity <cebotaricristian92@gmail.com>
2024-11-12 09:07:08 +0200

    v1.4.4

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer: React.FC = () => {
   return (
     <footer className="w-screen h-16 bg-[#28282E] flex items-center justify-center">
-      <h1 className="text-2xl font-bold text-[#80808E]">
+      <h1 className="text-xl font-bold text-[#80808E]">
         You also can find me on{" "}
         <a href="https://www.linkedin.com/in/cebotari-cristian-b28803192/" target="_blank">
           <span className="underline">LinkedIn</span>
